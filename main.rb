9f2add9f3e9cb15ea4a02c7df11d92d4acdd75df
Chevelle Boyer <chevelleboyer@gmail.com>
2021-05-10 22:55:18 -0700

    Apply move function to both lighten and darken_foreground

--- main.rb
+++ main.rb
@@ -15,10 +15,18 @@ def lighten_foreground(label_text, x, y, foreground_color)
 end
 
 class Paint
-	def initialize
+	def initialize(label_text, x, y, foreground_color)
+		@label_text = label_text
 		@x = x
 		@y = y
-		@label_text = label_text
 		@foreground_color = foreground_color
+	end
+
+	def darken_foreground
+		paint(@label_text, @x, @y, @foreground_color - 10, '#111111'))
+	end
+
+	def lighten_foreground
+		paint(@label_text, @x, @y, @foreground_color + 10, '#E0E0E0')
 	end
 end
